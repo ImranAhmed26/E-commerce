7be0b7c6c2d77fd8ad728df083961fce39b551b8
ImranAhmed26 <17-35401-3@student.aiub.edu>
2022-08-15 21:37:44 +0600

    small fixes

--- components/product-card.js
+++ components/product-card.js
@@ -3,11 +3,16 @@ import React from "react";
 
 import ProductImage from "../public/assets/images/55.jpg";
 
-const ProductCard = ({ name, image, price, sellPrice, stock }) => {
+const ProductCard = ({ name, productImage, price, sellPrice, stock }) => {
   return (
-    <div className=" lg:w-68 pt-6 px-10 bg-white text-gray-700 font-semibold rounded-md m-auto mt-2 drop-shadow-md">
-      <div>
-        <Image src={ProductImage} width={200} height={200} alt="img" />
+    <div className="w-68 pt-6 px-6 bg-white text-gray-700 font-semibold rounded-md m-auto mt-2 drop-shadow-md">
+      <div className="flex justify-center">
+        <Image
+          src={`https://idbdev.com/motion2/public/images/${productImage}`}
+          width={220}
+          height={200}
+          alt="img"
+        />
       </div>
       <div className="text-xl">{name}</div>
       <div className="flex justify-between py-3">
