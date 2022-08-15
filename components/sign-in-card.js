f7b1871ea467f45a9964dec71f8460ab6465dec2
ImranAhmed26 <17-35401-3@student.aiub.edu>
2022-08-15 20:28:42 +0600

    added the sign in page

--- components/sign-in-card.js
+++ components/sign-in-card.js
@@ -6,8 +6,8 @@ const SignInCard = () => {
   return (
     <div>
       <div className="flex flex-col w-80 h-96 bg-gray-100 mx-auto mt-20 rounded drop-shadow-md p-6">
-        <div className="font-bold text-2xl text-gray-700 px-6 py-5">Sign In</div>
-        <form className="px-6 py-10">
+        <div className="font-bold text-2xl text-gray-700 px-6 py-4">Sign In</div>
+        <form className="px-6 py-8">
           <div className="my ">
             {/* <h1>Email</h1> */}
             <input className="p-2 rounded w-full" placeholder="name@example.com" />
@@ -24,6 +24,8 @@ const SignInCard = () => {
             Sign In
           </button>
           <div className="pt-4" onClick={handleSubmit}>
+            Dont have an account?{" "}
+            <span className="font-semibold text-rose-400 cursor-pointer">Sign Up</span>
           </div>
         </form>
       </div>
